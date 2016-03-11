f405de33502334f77fb82085a5b1e398bf64c2b2
tumao <rchangchun@gmail.com>
2016-03-11 05:29:55 -0800

    test

--- app/controllers/Api/CallbackController.php
+++ app/controllers/Api/CallbackController.php
@@ -23,7 +23,7 @@ class CallbackController extends BaseController
 		// }else{
 		//   return false;
 		// }
-		$content = wp_file_get_contents ( 'php://input' );
+		$content = file_get_contents( 'php://input' );
 		$data = new \SimpleXMLElement ( $content );
 		\SeasLog::debug(json_encode($data));
 
