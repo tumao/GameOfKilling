60419e2c5d6b53a08c1e0b5fb2b72d291c8f1f4b
tumao <rchangchun@gmail.com>
2016-03-12 00:12:37 -0800

    xx

--- app/controllers/Admin/WeChat/WeChatConfController.php
+++ app/controllers/Admin/WeChat/WeChatConfController.php
@@ -22,7 +22,9 @@ class WeChatConfController extends ABaseController
 		$config_path = CONFIG_PATH.'/wx_menu.php';
 		$menus = require($config_path);
 		$token = $this->get_token();
-		$result = $this->sent_post("https://api.weixin.qq.com/cgi-bin/menu/create?access_token={$token}",json_encode($menus));
-		var_dump( $result);exit;
+		$menus = json_encode($menus);
+		\Seaslog::debug('menus__'.$menus);
+		$result = $this->sent_post("https://api.weixin.qq.com/cgi-bin/menu/create?access_token={$token}",$menus);
+		\Seaslog::debug('genmenu__'.json_encode($result));
 	}
 }
