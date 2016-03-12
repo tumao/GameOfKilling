ee32cd2b17267369f109e29bde5ae82ed1f0d0f7
tumao <rchangchun@gmail.com>
2016-03-12 05:36:09 -0800

    xx

--- app/controllers/Api/CallbackController.php
+++ app/controllers/Api/CallbackController.php
@@ -21,15 +21,54 @@ class CallbackController extends BaseController
 	    $result = $Weixin->checkSignature($timestamp, $nonce, $signature);
 	    \Seaslog::debug(json_encode($_REQUEST));	// 输入
 
-	    if($result)
-	    {
-	    	echo $echoStr;	// 校验签名成功，则返回$echostr,通知微信服务器校验成功
-	    	exit;
-	    }
-	    else
-	    {
-	    	echo false;
-	    	exit;
-	    }
+	    // if($result)
+	    // {
+	    // 	echo $echoStr;	// 校验签名成功，则返回$echostr,通知微信服务器校验成功
+	    // 	exit;
+	    // }
+	    // else
+	    // {
+	    // 	echo false;
+	    // 	exit;
+	    // }
+	    $this->responseMsg();
 	}
+
+
+	public function responseMsg()
+    {
+		//get post data, May be due to the different environments
+		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+      	//extract post data
+		if (!empty($postStr)){
+                
+              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
+                $time = time();
+                $textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";             
+				if(!empty( $keyword ))
+                {
+              		$msgType = "text";
+                	$contentStr = "Welcome to wechat world!";
+                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                	echo $resultStr;
+                }else{
+                	echo "Input something...";
+                }
+
+        }else {
+        	echo "";
+        	exit;
+        }
+    }
 }
--- config/wechat.php
+++ config/wechat.php
@@ -4,4 +4,5 @@ return [
 	'APPID'		=> 'wxd5ace7c14c8c927e',		// 微信的appid
 	'SECRET'	=> 'cdcaefcd4bcdea17699eaa5f220aa026',		// 微信的secret
 	'TOKEN'		=> 'quick',						// 接口配置中的token
+	'WXID'		=> 'gh_e39949e7ee04'			// 公众号的微信号
 ];
