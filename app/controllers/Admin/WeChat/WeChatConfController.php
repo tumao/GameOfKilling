8d73fc2ab47f5096259a7d82e3dab26901a7332a
tumao <rchangchun@gmail.com>
2016-03-12 00:53:22 -0800

    xx

--- app/controllers/Admin/WeChat/WeChatConfController.php
+++ app/controllers/Admin/WeChat/WeChatConfController.php
@@ -26,33 +26,10 @@ class WeChatConfController extends ABaseController
 		$config_path = CONFIG_PATH.'/wx_menu.php';
 		$menus = require($config_path);
 		$token = $this->get_token();
-		$menus = json_encode($menus,JSON_UNESCAPED_UNICODE);
-		// \Seaslog::debug('menus__'.$menus);
-		// $result = $this->sent_post("https://api.weixin.qq.com/cgi-bin/menu/create?access_token={$token}",$menus);
+		$menus = json_encode($menus,JSON_UNESCAPED_UNICODE);		// 第二个参数必不可少，意思是json_encode后中文不转码
+		$result = $this->sent_post("https://api.weixin.qq.com/cgi-bin/menu/create?access_token={$token}",$menus);
 		
 		$result = $this->create_menu($menus, $token);
 		\Seaslog::debug('genmenu__'.json_encode($result));
-	}
-
-	private function create_menu($data, $token)
-	{
-		header("Content-type: text/html; charset=utf-8");
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".$token);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 5.01; Windows NT 5.0)');
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$tmpInfo = curl_exec($ch);
-		if (curl_errno($ch)) {
-		  return curl_error($ch);
-		}
-
-		curl_close($ch);
-		return $tmpInfo;
 	}
 }
