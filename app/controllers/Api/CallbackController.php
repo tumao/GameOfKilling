ec6ef1dcc3d38d5b4c9268c0323d371a9a7cdf64
tumao <rchangchun@126.com>
2016-06-01 15:07:24 +0800

    xx

--- app/controllers/Api/CallbackController.php
+++ app/controllers/Api/CallbackController.php
@@ -27,54 +27,57 @@ class CallbackController extends BaseController
 	    			echo $echoStr;	// 校验签名成功，则返回$echostr,通知微信服务器校验成功
 	    			exit;
 	    		}
-	    	else
-	    	{
-	    		$Weixin->responseMsg();
-	    	}
+		    	else
+		    	{
+		    		$Weixin->responseMsg();
+		    	}
 	    	
-	    }
-	    else
-	    {
-	    	echo false;
-	    	exit;
-	    }
+	  	 }
+	    	else
+		{
+	    		echo false;
+		    	exit;
+		}
 	}
 
 
 	public function responseMsg()
-    {
+	{
 		//get post data, May be due to the different environments
 		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 		\Seaslog::debug('response__'.json_encode($postStr));
-      	//extract post data
-		if (!empty($postStr)){
+      		//extract post data
+		if (!empty($postStr))
+		{
                 
-              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $time = time();
-                $textTpl = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
-							<FuncFlag>0</FuncFlag>
-							</xml>";             
-				if(!empty( $keyword ))
-                {
-              		$msgType = "text";
-                	$contentStr = "Welcome to wechat world!";
-                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
-                }else{
-                	echo "Input something...";
-                }
+	              		$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+	               	$fromUsername = $postObj->FromUserName;
+	               	$toUsername = $postObj->ToUserName;
+	               	$keyword = trim($postObj->Content);
+	               	$time = time();
+	               	$textTpl = "<xml>
+					<ToUserName><![CDATA[%s]]></ToUserName>
+					<FromUserName><![CDATA[%s]]></FromUserName>
+					<CreateTime>%s</CreateTime>
+					<MsgType><![CDATA[%s]]></MsgType>
+					<Content><![CDATA[%s]]></Content>
+					<FuncFlag>0</FuncFlag>
+				</xml>"; 
 
-        }else {
-        	echo "";
-        	exit;
-        }
-    }
+			if(!empty( $keyword ))
+		               {
+		              		$msgType = "text";
+		                	$contentStr = "Hi, 你好啊!";
+		                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+		                	echo $resultStr;
+		               }else{
+		                	echo "Input something...";
+		               }
+	        	}
+	        	else
+	        	{
+	        		echo "";
+		        	exit;
+	        	}
+   	 }
 }
--- config/wechat.php
+++ config/wechat.php
@@ -5,9 +5,4 @@ return [
 	'SECRET'	=> 'cdcaefcd4bcdea17699eaa5f220aa026',		// 微信的secret
 	'TOKEN'	=> 'quick',						// 接口配置中的token
 	'WXID'		=> 'gh_e39949e7ee04'			// 公众号的微信号
-	// 'WXURL'	=> 'https://api.weixin.qq.com',	// 微信接口的地址
-	// 'APPID'		=> 'wx622901b799101c6b',		// 微信的appid
-	// 'SECRET'	=> '5a9f13147d17e4c6d6af28956d2e9fa0',		// 微信的secret
-	// 'TOKEN'	=> 'quick',						// 接口配置中的token
-	// 'WXID'		=> 'gh_cda1c64a8ba5'			// 公众号的微信号
 ];
