d07f73a3028413a08a2d5c51682204c691c71b05
tumao <rchangchun@126.com>
2016-06-30 08:03:17 +0800

    xx

--- app/controllers/Admin/WeChat/WeChatConfController.php
+++ app/controllers/Admin/WeChat/WeChatConfController.php
@@ -33,6 +33,7 @@ class WeChatConfController extends ABaseController
 		$token = $this->get_token();
 		$menus = json_encode($menus,JSON_UNESCAPED_UNICODE);		// 第二个参数必不可少，意思是json_encode后中文不转码
 		$result = $this->sent_post("https://api.weixin.qq.com/cgi-bin/menu/create?access_token={$token}",$menus);	// 发送post请求
+		\SeasLog::debug ('###genMenu###'. $result);
 		$data = json_decode ($result);						// 解析返回的json数据
 		if ($data->errcode == 0)
 		{
